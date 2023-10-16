926c1552e83615d0f93040416a63706cbc6c6519
Rafał Zubala <rzubala@gmail.com>
2023-10-16 14:50:54 +0200

    data model

--- src/main/java/guru/springframework/controllers/IndexController.java
+++ src/main/java/guru/springframework/controllers/IndexController.java
@@ -8,6 +8,7 @@ public class IndexController {
 
     @RequestMapping({"", "/", "/index"})
     public String getIndexPage() {
+        System.out.println("Some message to say... 123");
         return "index";
     }
 }
